9e5086cd8d86e7e0fe5bf58bb559f7c6ab51ad3e
J.Giang <jiangiang91@gmail.com>
2016-01-20 23:53:29 +0800

    - Focus username textbox onload

--- application/views/login.php
+++ application/views/login.php
@@ -4,37 +4,26 @@
     <meta charset="UTF-8">
     <title>EZY SYSTEM - Log in</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <!-- Bootstrap 3.3.4 -->
-    <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- Font Awesome Icons -->
-    <link href="<?php echo base_url();?>assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- Theme style -->
-    <link href="<?php echo base_url();?>assets/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-    <!-- iCheck -->
-    <link href="<?php echo base_url();?>assets/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
+  
+  <?php $this->view('templates/include_css'); ?>
+ 
+  
   <body class="login-page">
     <div class="login-box">
       <div class="login-logo">
         <a href="#"><b>EZY</b>Swimming</a>
       </div><!-- /.login-logo -->
       <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session.</p>
+        <p class="login-box-msg">Log in to start your session.</p>
         <?php echo validation_errors(); ?>
         <form action="<?php echo site_url();?>/login" method="post">
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="User Name" name="username"/>
+            <input type="text" class="form-control" placeholder="User Name" name="username" id="username" required />
             <span class="glyphicon glyphicon-user form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password" name="password"/>
+            <input type="password" class="form-control" placeholder="Password" name="password" id="password" required/>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div class="row">
@@ -54,6 +43,11 @@
     <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <!-- iCheck -->
     <script src="<?php echo base_url();?>assets/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
-    
+   
+    <script>
+      $(document).ready(function() {
+          $('#username').focus();
+          });
+  </script>
   </body>
 </html>
