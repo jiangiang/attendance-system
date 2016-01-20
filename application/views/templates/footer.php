8a846cf0fed2a57f543fb28c05774b83203300ca
J.Giang <jiangiang91@gmail.com>
2016-01-21 00:27:33 +0800

    remove link on footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,10 +5,6 @@
 	<strong>Copyright &copy; 2016 Ezy Swimming Academy, Malaysia.</strong>
 	All rights reserved.
 	<p>
-		Theme by <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.
-		Build with <a href="http://www.codeigniter.com/">CodeIgniter</a>.
-	</p>
-	<p>
 		Execution Time: <?php echo $this -> benchmark -> elapsed_time(); ?>;
 		Memory Used : <?php echo $this -> benchmark -> memory_usage(); ?>
 	</p>
