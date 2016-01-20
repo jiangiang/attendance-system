edceee68812b6eea47ff4537c90c364494cc79d5
J.Giang <jiangiang91@gmail.com>
2016-01-21 00:12:40 +0800

    - delete header.php - no more use - put css inside <head> tag

--- application/views/templates/main_lite.php
+++ application/views/templates/main_lite.php
@@ -4,9 +4,9 @@
 		<meta charset="UTF-8">
 		<title><?php echo $title; ?></title>
 		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <?php $this -> view('templates/include_css'); ?>
 	</head>
-	
-	<?php $this -> view('templates/include_css'); ?>
+    
   	<?php $this -> view('templates/include_js'); ?>
   	
 	<body class="layout-top-nav skin-green">
